9516cef01268f992cf8cd53224c57e1870799606
thanhf313 <thanhntpk03130@fpt.edu.vn>
2023-07-19 15:27:47 +0700

    TEST

--- app/src/main/java/com/example/asmandroid2/Screen/MainActivity.java
+++ app/src/main/java/com/example/asmandroid2/Screen/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.asmandroid2.Screen;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -10,7 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -19,6 +23,7 @@ import com.example.asmandroid2.dao.SanPhamDao;
 import com.example.asmandroid2.database.SanPhamAdapter;
 import com.example.asmandroid2.model.SanPham;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
@@ -62,20 +67,66 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
 
         // adapter đỗ dl
-        SanPhamAdapter sanPhamAdapter = new SanPhamAdapter(MainActivity.this,list,sanPhamDao);
+        SanPhamAdapter sanPhamAdapter = new SanPhamAdapter(MainActivity.this, list, sanPhamDao);
         recyclerView.setAdapter(sanPhamAdapter);
 
         // set up sk vào item tương ứng trg navigation
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.itLogout){
+                if (item.getItemId() == R.id.itLogout) {
                     startActivity(new Intent(MainActivity.this, LoginActivity.class));
                     Toast.makeText(MainActivity.this, "Thoát thành công", Toast.LENGTH_SHORT).show();
                 }
                 return true;
             }
         });
+        // bắt sk cho ic add
+        ic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // gọi trên chính nó nên kh cần gọi biến môi trường
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = ((MainActivity.this).getLayoutInflater());
+                View view = inflater.inflate(R.layout.dialog_add, null);
+                builder.setView(view);
+                AlertDialog alertDialog = builder.create();
 
+                TextInputEditText edtNameSP = view.findViewById(R.id.edtNameSP);
+                TextInputEditText edtPriceSP = view.findViewById(R.id.edtPriceSP);
+                TextInputEditText edtQuantitySP = view.findViewById(R.id.edtQuantitySP);
+
+                Button btnAdd = view.findViewById(R.id.btnAddSP);
+                btnAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String name = edtNameSP.getText().toString();
+                        String price = edtPriceSP.getText().toString();
+                        String quantity = edtQuantitySP.getText().toString();
+
+                        // kiểm tra việc nhập
+                        if(name.isEmpty() || price.isEmpty() || quantity.isEmpty()){
+                            Toast.makeText(MainActivity.this, "Vui lòng nhập dữ liệu", Toast.LENGTH_SHORT).show();
+                        }else {
+                            SanPham sanPham = new SanPham();
+                            sanPham.setTenSP(name);
+                            sanPham.setGiaSP(Integer.parseInt(price));
+                            sanPham.setSlSP(Integer.parseInt(quantity));
+
+                            boolean check = sanPhamDao.addSP(sanPham);
+                            if (check){
+                                Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+                                list.clear();
+                                list.addAll(sanPhamDao.getListSP());
+                                sanPhamAdapter.notifyDataSetChanged();
+                            }else {
+                                Toast.makeText(MainActivity.this, "Thêm thất bại ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+                alertDialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/asmandroid2/database/SanPhamAdapter.java
+++ app/src/main/java/com/example/asmandroid2/database/SanPhamAdapter.java
@@ -1,12 +1,16 @@
 package com.example.asmandroid2.database;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +19,7 @@ import com.example.asmandroid2.R;
 import com.example.asmandroid2.Screen.RegisterActivity;
 import com.example.asmandroid2.dao.SanPhamDao;
 import com.example.asmandroid2.model.SanPham;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
@@ -44,6 +49,104 @@ public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.ViewHodl
         holder.tvGiaSP.setText(String.valueOf(list.get(position).getGiaSP())+ "VNĐ -");
         holder.tvSLSP.setText("SL: "+ String.valueOf(list.get(position).getSlSP()));
 
+        // bắt sự kiện delete
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Cảnh báo");
+                builder.setIcon(R.drawable.baseline_warning_amber_24);
+                builder.setMessage("Bạn có chắc chắn muốn xóa sản phẩm ' " + list.get(holder.getAdapterPosition()).getTenSP() + "' Không?");
+                builder.setPositiveButton("Đông ý", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int masp = list.get(holder.getAdapterPosition()).getMaSP();
+                        Boolean check = sanPhamDao.deleteSP(masp);
+                        if (check){
+                            Toast.makeText(context, "Xóa thành công", Toast.LENGTH_SHORT).show();
+                            list.clear();
+                            list = sanPhamDao.getListSP();
+                            notifyItemRemoved(holder.getAdapterPosition());
+                        }else {
+                            Toast.makeText(context, "Xóa thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        // sk edit
+        holder.imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SanPham sanPham = list.get(holder.getAdapterPosition()); // khởi tạo
+                dialogUpdateSP(sanPham);
+            }
+        });
+    }
+
+    private void dialogUpdateSP(SanPham sanPhamUpdate) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        View view =inflater.inflate(R.layout.dialog_edit,null);
+        builder.setView(view);
+        AlertDialog dialog = builder.create();
+
+        // ánh xạ các widget
+        TextInputEditText edtNameSPEdit= view.findViewById(R.id.edtEditNameSP);
+        TextInputEditText edtPriceSPEdit = view.findViewById(R.id.edtEditPriceSP);
+        TextInputEditText edtQuantitySPEdit = view.findViewById(R.id.edtEditQuantitySP);
+        Button btnUpdate = view.findViewById(R.id.btnUpdateSP);
+        Button btnCancel = view.findViewById(R.id.btnCancelSP);
+
+        // set dữ liệu lên các edit để lấy giá trị cữ của sp can update
+        edtNameSPEdit.setText(sanPhamUpdate.getTenSP());
+        edtPriceSPEdit.setText(String.valueOf(sanPhamUpdate.getGiaSP()));
+        edtQuantitySPEdit.setText(String.valueOf(sanPhamUpdate.getSlSP()));
+
+        // bắt sk
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // lấy giá trị
+                String name = edtNameSPEdit.getText().toString();
+                String price = edtPriceSPEdit.getText().toString();
+                String  quantity = edtQuantitySPEdit.getText().toString();
+
+                SanPham sp = new SanPham();
+                sp.setMaSP(sanPhamUpdate.getMaSP());
+                sp.setTenSP(name);
+                sp.setGiaSP(Integer.parseInt(price));
+                sp.setSlSP(Integer.parseInt(quantity));
+
+                boolean check = sanPhamDao.updateSP(sp);
+                if (check){
+                    Toast.makeText(context, "Cập nhật thành công", Toast.LENGTH_SHORT).show();
+                    list.clear();
+                    list = sanPhamDao.getListSP();
+                    // đóng dialog
+                    dialog.dismiss();
+                }else {
+                    Toast.makeText(context, "Cập nhật thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        // showw dialog khi goi
+        dialog.show();
     }
 
     @Override
